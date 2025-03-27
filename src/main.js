1ec366012d37cecde2de83c9ae6f51ecb6ec1479
Elijah <elijah.macharia@student.moringaschool.com>
2025-03-27 09:38:58 +0300

    phase-1-project

---
+++ src/main.js
@@ -0,0 +1,83 @@
+let shop = document.getElementById("shop");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+let shopItemsData = [];
+
+// Fetch JSON data
+fetch("http://localhost:3000/clothes") 
+  .then((response) => response.json())
+  .then((data) => {
+    shopItemsData = data;
+    generateShop(); // Call generateShop after data is fetched
+  })
+  .catch((error) => console.error("Error fetching data:", error));
+
+let generateShop = () => {
+  if (!shopItemsData.length) return;
+  
+  shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, desc, img, price } = x;
+      let search = basket.find((y) => y.id === id) || [];
+      return `
+        <div id=product-id-${id} class="item">
+          <img width="220" src=${img} alt="">
+          <div class="details">
+            <h3>${name}</h3>
+            <p>${desc}</p>
+            <div class="price-quantity">
+              <h2>Ksh ${price} </h2>
+              <div class="buttons">
+                <i onclick="decrement('${id}')" class="bi bi-dash-lg"></i>
+                <div id=${id} class="quantity">${search.item === undefined ? 0 : search.item}</div>
+                <i onclick="increment('${id}')" class="bi bi-plus-lg"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+    })
+    .join("");
+};
+
+let increment = (id) => {
+  let search = basket.find((x) => x.id === id);
+
+  if (search === undefined) {
+    basket.push({
+      id: id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  update(id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let decrement = (id) => {
+  let search = basket.find((x) => x.id === id);
+
+  if (!search) return;
+  if (search.item === 0) return;
+
+  search.item -= 1;
+  update(id);
+
+  basket = basket.filter((x) => x.item !== 0);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search ? search.item : 0;
+  calculation();
+};
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
